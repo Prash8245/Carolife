1b428b68c3605d20bc70e945e05086b3659734f5
Chitrashree23 <chitrashree.20201cbc0014@presidencyuniversity.in>
2023-11-21 21:48:21 +0530

    Signup Page

--- WebApp/src/App.js
+++ WebApp/src/App.js
@@ -1,11 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import Register from './Components/Authorized/Register';
 import Home from './Components/Home/Home';
+import {BrowserRouter, Route, Routes}from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+      <Routes>
+      <Route exact path='/' element={<Home/>}/>
+          <Route path='/login'  element={<Register/>} /> 
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
